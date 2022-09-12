8f462ae7af8fea1715076c7419e3d2d3699e57f4
Michael Sproul <michael@sigmaprime.io>
2022-09-12 17:11:32 +1000

    Build empty payloads without registration

--- src/noop_builder.rs
+++ src/noop_builder.rs
@@ -16,7 +16,9 @@ use parking_lot::RwLock;
 use ssz_rs::Merkleized;
 use std::collections::HashMap;
 use std::sync::Arc;
-use types::{ChainSpec, EthSpec, ExecutionBlockHash};
+use types::{Address, ChainSpec, EthSpec};
+
+const DEFAULT_GAS_LIMIT: u64 = 30_000_000;
 
 #[derive(Clone)]
 pub struct NoOpBuilder<E: EthSpec> {
@@ -26,10 +28,21 @@ pub struct NoOpBuilder<E: EthSpec> {
     payload_cache: Arc<PayloadCache<E>>,
     val_registration_cache: Arc<RwLock<HashMap<BlsPublicKey, SignedValidatorRegistration>>>,
     builder_sk: SecretKey,
+    config: NoOpConfig,
+}
+
+#[derive(Clone)]
+pub struct NoOpConfig {
+    pub default_fee_recipient: Option<Address>,
 }
 
 impl<E: EthSpec> NoOpBuilder<E> {
-    pub fn new(beacon_client: BeaconNodeHttpClient, spec: ChainSpec, context: Context) -> Self {
+    pub fn new(
+        beacon_client: BeaconNodeHttpClient,
+        spec: ChainSpec,
+        context: Context,
+        config: NoOpConfig,
+    ) -> Self {
         let sk = SecretKey::random(&mut rand::thread_rng()).unwrap();
         Self {
             beacon_client,
@@ -38,6 +51,7 @@ impl<E: EthSpec> NoOpBuilder<E> {
             val_registration_cache: Arc::new(RwLock::new(HashMap::new())),
             payload_cache: Arc::new(PayloadCache::default()),
             builder_sk: sk,
+            config,
         }
     }
 
@@ -74,28 +88,46 @@ impl<E: EthSpec> BlindedBlockProvider for NoOpBuilder<E> {
         &self,
         bid_request: &BidRequest,
     ) -> Result<SignedBuilderBid, BlindedBlockProviderError> {
-        let signed_cached_data = self
+        let (fee_recipient, gas_limit) = self
             .val_registration_cache
             .read()
             .get(&bid_request.public_key)
-            .ok_or_else(|| convert_err("missing registration"))?
-            .clone();
-        let cached_data = signed_cached_data.message;
-        let fee_recipient = from_ssz_rs(&cached_data.fee_recipient)?;
+            .map_or_else(
+                || {
+                    self.config
+                        .default_fee_recipient
+                        .map(|fee_recipient| (fee_recipient, DEFAULT_GAS_LIMIT))
+                        .ok_or_else(|| {
+                            BlindedBlockProviderError::Custom(format!(
+                                "missing registration and no default fee recipient set"
+                            ))
+                        })
+                },
+                |registration| {
+                    let fee_recipient = from_ssz_rs(&registration.message.fee_recipient)?;
+                    let gas_limit = registration.message.gas_limit;
+                    Ok((fee_recipient, gas_limit))
+                },
+            )?;
 
-        let (payload_attributes, forkchoice_update_params, block_number) =
+        // FIXME(sproul): this takes 2s+ on Goerli
+        //
+        // really we just need:
+        // - prev_randao (from head state)
+        // - block_number (parent block + 1)
+        tracing::info!("requesting parameters from BN");
+        let (payload_attributes, _, block_number) =
             get_params::<E>(&self.beacon_client, bid_request, fee_recipient, &self.spec).await?;
+        tracing::info!("obtained parameters from BN");
 
         let payload = FullPayload {
             execution_payload: ExecutionPayload {
-                parent_hash: forkchoice_update_params
-                    .head_hash
-                    .unwrap_or(ExecutionBlockHash::zero()),
+                parent_hash: from_ssz_rs(&bid_request.parent_hash)?,
                 timestamp: payload_attributes.timestamp,
                 fee_recipient: fee_recipient,
                 prev_randao: payload_attributes.prev_randao,
                 block_number,
-                gas_limit: cached_data.gas_limit,
+                gas_limit,
                 ..Default::default()
             },
         };
